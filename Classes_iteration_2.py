a3eab9c09ad4baec6fc63b79d935667a325923fd
Simon Gram Gregersen <simon@MacBook-Pro-tilhrende-Simon.local>
2022-03-28 15:31:31 +0200

    Udkast til classes

--- Classes_iteration_2.py
+++ Classes_iteration_2.py
@@ -1,23 +1,62 @@
 #constructor - hvad skal der til for at lave en employee
 
-class user():
+from datetime import datetime
+
+
+class User():
     def __init__(self, name, UserSchedule, PersonalInfo, ContactInfo):
         self.name = name
         self.UserSchedule = UserSchedule
         self.PersonalInfo = PersonalInfo
         self.Contactinfo = ContactInfo
 
-    def set_name():
-            pass
+    # def set_name():
+    #         pass
 
-    def get_name():
-            pass
+    # def get_name():
+    #         pass
+    #Tror ikke ovenstående er nødvendige
 
-class teacher(user):
-    def __init__(self, CourseAffiliations):
+class Teacher(User):
+    def __init__(self, CourseAffiliations: list):
         self.CourseAffiliations = CourseAffiliations
 
-    def propose_schedule():
+    def proposeSchedule():
         pass
 
+class Course():
+    def __init__(self, Schedule, ECTS, CourseID, ER, Responsible) -> None:
+        self.Schedule = Schedule
+        self.ECTS = ECTS
+        self.CourseID = CourseID
+        self.EnrolledStudents = ER
+        self.Responsible = Responsible
+
+class Schedule():
+    def __init__(self, Classes, Date: datetime) -> None:
+        self.Classes = Classes
+        self.Date = Date
     
+    def __init__(self, TimeFrame, Date: datetime) -> None:
+        self.TimeFrame = TimeFrame
+        self.Date = Date
+
+
+    def updateSchedule():
+        pass
+
+    def viewSchedule():
+        pass
+
+class TimeFrame():
+    def __init__(self, StartTime: datetime, EndTime: datetime) -> None:
+        self.StartTime = StartTime
+        self.EndTime = EndTime
+    
+    def updateTimeFrame():
+        pass
+
+    def deleteTimeFrame():
+        pass
+    
+
