86048694b74ec7f2bea9bbb6f1316fbdb7edceab
Simon Gram Gregersen <simon@mbptilhndesimon.lan>
2022-04-22 10:14:00 +0200

    fixed classes and wrote unit tests

--- Controller.py
+++ Controller.py
@@ -2,4 +2,7 @@ import Classes_iteration_2
 
 mette = Classes_iteration_2.Teacher("Mette", "g", "s", "Teacher", [])
 metteSchedule = mette.proposeSchedule(["Mandag 12-14", "Tirsdag 10-17", "Onsdag 15-17", "Torsdag 8-15"])
-print(metteSchedule)
+# metteSchedule2 = mette.proposeSchedule(["Test"])
+# print(metteSchedule2)
+
+# print(type(metteSchedule["Mette"]))
