f359e2ac54fdb2b3b4dd37f25dc615a46d15aace
ips5 <idasuhr@hotmail.com>
2022-05-25 22:19:37 +0200

    ny gui opbygning

--- gui/main.py
+++ gui/main.py
@@ -1,9 +1,9 @@
-from samletUi import *
+from loginUi import *
 import sys
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    window = samletUi()
+    window = loginUi()
     window.show()
     app.exec()
