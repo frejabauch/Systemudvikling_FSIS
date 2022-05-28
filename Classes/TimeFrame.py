a452bc7afc5e56b16c4f2c8d316f51646f41a40b
Simon Gram Gregersen <simongram@me.com>
2022-05-28 10:32:35 +0200

    Update timeframe

--- Classes/TimeFrame.py
+++ Classes/TimeFrame.py
@@ -1,5 +1,11 @@
 from datetime import datetime
 from enum import Enum
+from Location import Location
+
+class TimeFrameType(Enum):
+    Lecture = 1
+    Class = 2
+    Meeting = 3
 
 class Day(Enum):
     Mon = "Mon"
@@ -10,13 +16,14 @@ class Day(Enum):
 
 # class TimeFrameBuilder():
 
-
 class TimeFrame():
-    def __init__(self, ScheduleID: int, StartTime: datetime, EndTime: datetime, Weekday: Day):
+    def __init__(self, ScheduleID: int, StartTime: datetime, EndTime: datetime, Weekday: Day, ClassLocation: Location, ClassType: TimeFrameType):
         self.ScheduleID = ScheduleID
         self.StartTime = StartTime
         self.EndTime = EndTime
         self.Weekday = Weekday
+        self.ClassLocation = ClassLocation
+        self.ClassType = ClassType
     
     def updateTimeFrame():
         pass
